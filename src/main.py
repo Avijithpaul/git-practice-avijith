f1dfe4497846466ff0f903c6c0a6270471452841
Avijith paul <avijithpaul2006@gmail.com>
2026-06-19 21:42:46 +0600

    Add basic calculator functions for addition and subtraction

--- src/main.py
+++ src/main.py
@@ -1,2 +1,7 @@
+from utils import add, subtract
+
 print("Name: Avijith Paul")
 print("Today's Date: June 19, 2026")
+
+print("Addition Result:", add(10, 5))
+print("Subtraction Result:", subtract(10, 5))
