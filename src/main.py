6f2f24ad3524b997106d7cf0f17803f06e2abf15
Avijith paul <avijithpaul2006@gmail.com>
2026-06-19 21:49:44 +0600

    Add multiplication function to utils and update main program

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Avijith Paul")
 print("Today's Date: June 19, 2026")
 
 print("Addition Result:", add(10, 5))
 print("Subtraction Result:", subtract(10, 5))
+print("Multiplication Result:", multiply(10, 5))
